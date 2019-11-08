85f5f87a1c3e462219a1001a418c3c9da8d369a7
51998138 <alexandersevruk96@gmail.com>
2019-11-08 18:26:44 +0200

    Updates

---
+++ precache-manifest.d39a8abe9b2d29725ae3ff7798311904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608f21b5eb84a202092627632cc52010",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "12d7b9049d12bc88a631",
+    "url": "/Portfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "3458b3339b0274308ff2",
+    "url": "/Portfolio/static/css/main.ef61bc05.chunk.css"
+  },
+  {
+    "revision": "12d7b9049d12bc88a631",
+    "url": "/Portfolio/static/js/2.f21d3082.chunk.js"
+  },
+  {
+    "revision": "3458b3339b0274308ff2",
+    "url": "/Portfolio/static/js/main.cd4158d9.chunk.js"
+  },
+  {
+    "revision": "4cb671534b2d867f6e53",
+    "url": "/Portfolio/static/js/runtime-main.5988fdac.js"
+  }
+]);
