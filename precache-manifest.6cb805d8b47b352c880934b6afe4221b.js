0d7e6893166be6016ec72613a6cd85b449427e8d
51998138 <alexandersevruk96@gmail.com>
2020-01-26 14:48:09 +0200

    Updates

--- precache-manifest.6cb805d8b47b352c880934b6afe4221b.js
+++ precache-manifest.6cb805d8b47b352c880934b6afe4221b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8cd2639e631c588beca8d9fc8e6aa975",
+    "revision": "4dc59117748306838501843ae5639098",
     "url": "/Portfolio/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Portfolio/static/css/2.04aae6c4.chunk.css"
   },
   {
-    "revision": "8923da71638945633671",
-    "url": "/Portfolio/static/css/main.8b602752.chunk.css"
+    "revision": "e1f7deb222775f8f4520",
+    "url": "/Portfolio/static/css/main.75b7723d.chunk.css"
   },
   {
     "revision": "12d7b9049d12bc88a631",
     "url": "/Portfolio/static/js/2.f21d3082.chunk.js"
   },
   {
-    "revision": "8923da71638945633671",
-    "url": "/Portfolio/static/js/main.fe27f30e.chunk.js"
+    "revision": "e1f7deb222775f8f4520",
+    "url": "/Portfolio/static/js/main.f59e07b7.chunk.js"
   },
   {
     "revision": "4cb671534b2d867f6e53",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.2174f246bc3f2c4c11de9ec413934626.js"
+  "/Portfolio/precache-manifest.6cb805d8b47b352c880934b6afe4221b.js"
 );
 
 self.addEventListener('message', (event) => {
